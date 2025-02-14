5034d51e290aaab0e81830f2ea682cd8a4502414
Euler <eulerborgesf@gmail.com>
2025-02-14 15:33:04 -0300

    quase?

--- ReconDigitos/Perceptrons.py
+++ ReconDigitos/Perceptrons.py
@@ -1,6 +1,34 @@
 class Perceptron:
     def __init__(self, alvo):
-        self.pesos = [0] * 49
+        self.pesos_atuais = [0] * 49
+        self.pesos_passados = [0] * 49
         self.bias = 0
+        self.bias_passado = 0
         self.alpha = 1
         self.alvo = alvo
+    
+    #representa uma iteração do passo 2
+    def treinar_perceptron(self, digito):
+        saida_teorica = 1 if digito == self.alvo else -1
+        #saida não bipolar
+        saida_atual_st = self.bias + sum([digito[i] * self.pesos_atuais[i] for i in range(len(digito))])
+  
+        #saida bipolar
+        saida_atual_ct = 1 if saida_atual_st >= 0 else -1
+
+        if saida_atual_ct != saida_teorica:
+            #atualizando bias
+            self.bias += self.alpha * saida_teorica
+
+            #atualizando pesos
+            for i in range(len(digito)):
+                self.pesos_atuais[i] += self.alpha * digito[i] * saida_teorica
+
+
+    def atualizar_pesos_passados(self):
+        self.pesos_passados = self.pesos_atuais
+        self.bias_passado = self.bias
+
+
+    def teste_de_igualdade(self):
+        return self.pesos_atuais == self.pesos_passados and self.bias == self.bias_passado
--- ReconDigitos/recon.py
+++ ReconDigitos/recon.py
@@ -1,3 +1,30 @@
-import digits_bitmap
-import Perceptrons
+from digits_bitmap import digits_bitmap
+from Perceptrons import Perceptron as perceptron
+
+PERCEPTRONS = []
+
+def main():
+    #inicia os perceptrons necessários
+    cria_perceptrons()
+
+    #treina os perceptrons
+    for perceptron in PERCEPTRONS:
+        while True:
+            for digit in digits_bitmap:
+                perceptron.treinar_perceptron(digits_bitmap[digit])
+            if perceptron.teste_de_igualdade():
+                break
+
+
+
+
+
+
+
+def cria_perceptrons():
+    for digit in digits_bitmap:
+        PERCEPTRONS.append(perceptron(digits_bitmap[digit]))
+
+if __name__ == "__main__":
+    main()
 
